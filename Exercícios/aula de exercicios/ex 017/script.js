88dc71a20db7aa0e216d372a00ec97e62d58f9d7
Gustavo <gustavosemmler@hotmail.com>
2023-09-15 18:18:55 -0300

    ex de js

--- Exercícios/aula de exercicios/ex 017/script.js
+++ Exercícios/aula de exercicios/ex 017/script.js
@@ -1,1 +1,18 @@
-alert('ola')
+function gerar() {
+    let num = document.getElementById('in')
+    let tab = document.getElementById('it')
+    if (num.value.length == 0) {
+        alert('por favor, digite um numero')
+    } else {
+        let n = Number(num.value)
+        let c = 0 
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} * ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
